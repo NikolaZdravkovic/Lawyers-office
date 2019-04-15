e2fec5fafb7d4db045ffebbc86092b25a2effc8d
NikolaZdravkovic <zdravko.dzoni@gmail.com>
2019-04-15 20:00:08 +0200

    Added some local storage to quick form, also made some small changes in html/css quick form as well

--- app.js
+++ app.js
@@ -74,7 +74,9 @@ jQuery(function ($) {
 	//Click event to scroll to top
 
 	$('.scrollToTop').on('click', function () {
-		$('html, body').animate({ scrollTop: 0 }, 800);
+		$('html, body').animate({
+			scrollTop: 0
+		}, 800);
 		return false;
 	});
 
@@ -84,26 +86,26 @@ jQuery(function ($) {
 	$(window).on('load', function () { // makes sure the whole site is loaded
 		$('#status').fadeOut(); // will first fade out the loading animation
 		$('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-		$('body').delay(100).css({ 'overflow': 'visible' });
+		$('body').delay(100).css({
+			'overflow': 'visible'
+		});
 	})
 
 
-	
+
 	/* 6. WOW ANIMATION */
 
-	wow = new WOW(
-		{
-			animateClass: 'animated',
-			offset: 100,
-			live: true,
+	wow = new WOW({
+		animateClass: 'animated',
+		offset: 100,
+		live: true,
 
-		}
-	);
+	});
 	wow.init();
 
 });
 
-	/* 7. GOOGLE MAP */
+/* 7. GOOGLE MAP */
 
 
 function initMap() {
@@ -121,7 +123,7 @@ function initMap() {
 	});
 }
 
-	/* 8. SLICK SLIDER(BLOG) */
+/* 8. SLICK SLIDER(BLOG) */
 
 $(function () {
 	$('.slider').slick({
@@ -158,13 +160,79 @@ $(function () {
 				slidesToScroll: 1
 			}
 		}
-			// You can unslick at a given breakpoint now by adding:
-			// settings: "unslick"
-			// instead of a settings object
+
 		]
 	});
 });
 
 
+/* 9. LOCAL STORAGE FOR QUICK FORM */
+
+const email = document.getElementById('form_email');
+const message = document.getElementById('form_message');
+const modal = document.getElementById('id01');
 
 
+//Event listner
+
+document.getElementById('form').addEventListener('submit', localStr)
+
+
+// Check email(regex)
+checkEmail = () => {
+
+	var filter = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+	if (!filter.test(email.value)) {
+		alert('please enter bla bla')
+		email.value = '';
+		return false;
+	}
+	getData();
+}
+
+// Local storage (setData and getData)
+getData = () => {
+
+	localStorage.setItem('message', message.value)
+	localStorage.setItem("email", email.value);
+	document.getElementById('showEmail').innerHTML = localStorage.getItem('email');
+	document.getElementById('showMessage').innerHTML = localStorage.getItem('message');
+
+	modal.style.display = 'block';
+
+}
+// Cancel function
+cancel = () => {
+	localStorage.clear();
+	modal.style.display = "none";
+}
+// Confrim message function
+sendMsg = () => {
+
+	// show loader
+	document.getElementById("question").style.display = "none";
+	document.getElementById('loading').style.display = "block";
+	// modal.style.display = "none";
+	setTimeout(endMsg, 2000);
+}
+
+
+// Local storage function
+
+function localStr(e) {
+
+	checkEmail();
+
+	e.preventDefault();
+}
+
+endMsg = () => {
+	document.getElementById('showEnd').style.display = "block";
+	document.getElementById('loading').style.display = "none";
+
+}
+
+ok = () => {
+	document.getElementById('id01').style.display = 'none';
+	window.location.reload()
+}
