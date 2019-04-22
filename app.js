4edb4092ceced653122e7d0bc3bfac5681e57281
NikolaZdravkovic <zdravko.dzoni@gmail.com>
2019-04-22 23:36:21 +0200

    added single page/modified css

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ jQuery(function ($) {
 
 	/* 5. PRELOADER */
 
-	$(window).on('load', function () { // makes sure the whole site is loaded
+	$(function () { // makes sure the whole site is loaded
 		$('#status').fadeOut(); // will first fade out the loading animation
 		$('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
 		$('body').delay(100).css({
