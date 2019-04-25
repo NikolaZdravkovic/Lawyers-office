05da8480ac2ac71059c90ec5e0c75ac0b9fa213b
NikolaZdravkovic <zdravko.dzoni@gmail.com>
2019-04-26 00:39:50 +0200

    modified rating system

--- app.js
+++ app.js
@@ -168,79 +168,6 @@ $(function () {
 
 /* 9. LOCAL STORAGE FOR QUICK FORM */
 
-// const email = document.getElementById('form_email');
-// const message = document.getElementById('form_message');
-// const modal = document.getElementById('id01');
-
-
-// //Event listner
-
-// document.getElementById('form').addEventListener('submit', localStr)
-
-
-// // Check email(regex)
-// checkEmail = () => {
-
-// 	var filter = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-// 	if (!filter.test(email.value)) {
-// 		alert('please enter bla bla')
-// 		email.value = '';
-// 		return false;
-// 	}
-// 	getData();
-// }
-
-// // Local storage (setData and getData)
-// getData = () => {
-
-// 	localStorage.setItem('message', message.value)
-// 	localStorage.setItem("email", email.value);
-// 	document.getElementById('showEmail').innerHTML = localStorage.getItem('email');
-// 	document.getElementById('showMessage').innerHTML = localStorage.getItem('message');
-
-// 	modal.style.display = 'block';
-
-// }
-// // Cancel function
-// cancel = () => {
-// 	localStorage.clear();
-// 	modal.style.display = "none";
-// }
-// // Confrim message function
-// sendMsg = () => {
-
-// 	// show loader
-// 	document.getElementById("question").style.display = "none";
-// 	document.getElementById('loading').style.display = "block";
-// 	// modal.style.display = "none";
-// 	setTimeout(endMsg, 2000);
-// }
-
-
-// // Local storage function
-
-// function localStr(e) {
-
-// 	checkEmail();
-
-// 	e.preventDefault();
-// }
-
-// endMsg = () => {
-// 	document.getElementById('showEnd').style.display = "block";
-// 	document.getElementById('loading').style.display = "none";
-
-// }
-
-// ok = () => {
-// 	document.getElementById('id01').style.display = 'none';
-// 	window.location.reload()
-// }
-
-
-
-
-
 
 
 document.getElementById("btn").addEventListener("click", signup);
@@ -258,7 +185,7 @@ function signup(event) {
 
 }
 
-function save(email, message) {
+function saveUser(email, message) {
 	const newUser = {
 		email: email,
 		message: message,
@@ -300,7 +227,7 @@ validation = () => {
 		alert('please enter bla bla')
 		// return false;
 	} else { // ukoliko user ne prodje validaciju, ne ispiujemo ga
-		save(email.value, message.value);
+		saveUser(email.value, message.value);
 	}
 }
 
@@ -333,4 +260,167 @@ const ok = () => {
 }
 
 
+
+
+
+
+// Adding review
+
+
+const btn = document.getElementById("sbmBtn");
+const name = document.getElementById("name");
+const surname = document.getElementById("surname");
+const email2 = document.getElementById("emailRev");
+const text = document.getElementById("text");
+const cards = document.getElementById("cards");
+const form = document.querySelector('#task-form');
+
+// DOM Load event
+document.addEventListener('DOMContentLoaded', getTasks);
+// Add task event
+form.addEventListener('submit', addTask);
+
+// Get Tasks from LS
+function getTasks() {
+
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function (newComm) {
+
+        const div = document.createElement('div');
+        div.className = "testimonial";
+
+        const div2 = document.createElement('div');
+        div2.className = "pic";
+
+        div.appendChild(div2);
+
+        const img = document.createElement('img');
+        img.src = "tormund.png";
+
+        div2.appendChild(img);
+
+        const p = document.createElement('p');
+        p.className = "description";
+        p.innerHTML = newComm.text;
+
+        div.appendChild(p);
+
+        const h3 = document.createElement('h3');
+
+        h3.className = "title";
+        h3.innerHTML = newComm.name;
+        div.appendChild(h3);
+
+
+        cards.appendChild(div);
+
+
+
+    });
+}
+
+
+
+
+// Add Task
+function addTask(e) {
+
+    const filter = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    const filterNameSurname = /[a-zA-Z]/
+    if (!filter.test(email2.value)) {
+        alert('please enter valid mail')
+        e.stopPropagation()
+
+    } if (!filterNameSurname.test(name.value)) {
+        alert("no numbers")
+        e.stopPropagation()
+    }
+    if (!filterNameSurname.test(surname.value)) {
+        alert("no numbers")
+        e.stopPropagation()
+
+    }
+    else { // ukoliko user ne prodje validaciju, ne ispiujemo ga
+        save(name.value, surname.value, email2.value, text.value);
+
+        const div = document.createElement('div');
+        div.className = "testimonial";
+
+        const div2 = document.createElement('div');
+        div2.className = "pic";
+
+        div.appendChild(div2);
+
+        const img = document.createElement('img');
+        img.src = "tormund.png";
+
+        div2.appendChild(img);
+
+        const p = document.createElement('p');
+        p.className = "description";
+        p.appendChild(document.createTextNode(text.value));
+
+        div.appendChild(p);
+
+        const h3 = document.createElement('h3');
+
+        h3.className = "title";
+        h3.appendChild(document.createTextNode(name.value));
+        div.appendChild(h3);
+
+
+        cards.appendChild(div);
+
+    }
+
+
+    e.preventDefault();
+}
+
+
+
+// Store Task
+function save(name, surname, email2, text) {
+
+    let tasks = [];
+
+
+
+    const newComm = {
+        name: name,
+        surname: surname,
+        email2: email2,
+        text: text
+    };
+
+    if (localStorage.getItem("tasks")) {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+
+
+
+
+        if (!tasks.find(comm => comm.email2 === newComm.email2)) {
+            tasks.push(newComm);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            alert(`User successfully registered!`);
+            // location.assign("login.html");
+        } else {
+            alert(`User ${email2} already exist!`);
+        }
+    } else { // Ovo se izvrsava samo prvi put, tj. samo kada u localStorage-u
+        // ne postoji polje/kljuc "users"
+        tasks.push(newComm);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        alert(`User successfully registered!`);
+        // location.assign("login.html");
+    }
+
+
+}
 
